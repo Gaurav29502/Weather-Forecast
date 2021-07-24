eb3169d770aac7a875a1e8f043d7e9f1db31462e
Gaurav M Nenwani <65184110+Gaurav29502@users.noreply.github.com>
2021-07-24 23:51:58 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,17 @@
+#import the necessary package!
+import requests
+
+#input the city name
+city = 'thane'
+print(city)
+
+
+#Display the message!
+print('Displaying Weater report for: ' + city)
+
+#fetch the weater details
+url = 'https://wttr.in/{}'.format(city)
+res = requests.get(url)
+
+#display the result!
+print(res.text)
